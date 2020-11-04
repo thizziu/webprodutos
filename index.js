26e1d1602d5cbf851278140a025db2e06f09348f
Thiago <thisantossp10@gmail.com>
2020-11-04 09:45:47 -0300

    Adição de arquivo .gitignore

--- index.js
+++ index.js
@@ -14,19 +14,50 @@ const app = express();
 const bodyParser = require("body-parser");
 
 //Vamos usar o body-parser
-app.use(bodyParser);
-app.post("/dados",bodyParser, (req,res) => );
-    res.send(req,body);
-});
+app.use(bodyParser.json());
 
 // Vamos iniciar os exemplos
 // de utilização de verbos HTTP
 
+var layout = [
+  {
+    header: "loja de produtos",
+    navegacao: "listar, cadastrar, atualizar, deletar",
+    main: "pagina do corpo",
+    footer: "Av. João Paulo, 45, Vila Nova - São Paulo - SP",
+  },
+];
+
 // GET
 // Quando o meu usuario deseja obter
 // algum dado do servidor.
-app.get("/dados", (req, res) => {
-  res.send("Você está no verbo GET");
+app.get("/listar", (req, res) => {
+  layout[0].main = [
+    {
+      nome: "Calça",
+      descricao: "Jeans",
+      preco: "R$ 150",
+      imagem:
+        "https://s.modab.com.br/catalog/product/cache/1/small_image/600x900/9df78eab33525d08d6e5fb8d27136e95/c/a/calca-jeans-modeladora-mega-bubum-03.jpg",
+    },
+
+    {
+      nome: "Blusa",
+      descricao: "Branca",
+      preco: "R$ 50",
+      imagem:
+        " https://images.tcdn.com.br/img/img_prod/698668/blusa_pkd_manga_curta_com_gola_branca_607_1_20190621140744.jpg ",
+    },
+
+    {
+      nome: "Tenis",
+      descricao: "Nike",
+      preco: "R$ 400",
+      imagem:
+        "https://artwalk.vteximg.com.br/arquivos/ids/214041-1000-1000/Tenis-Nike-Air-Force-1-07-Masculino-Branco.jpg?v=636975787616830000",
+    },
+  ];
+  res.send(layout);
 });
 
 //POST
@@ -34,7 +65,7 @@ app.get("/dados", (req, res) => {
 // ao servidor com intuito de cadastrar ou
 // realizar autenticação
 app.post("/dados", (req, res) => {
-  res.send("req.body");
+  res.send(req.body);
 });
 
 //PUT
